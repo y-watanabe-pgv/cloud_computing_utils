430cef12d88e450ca445f2da3b17c561f68354a4
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-09-01 00:04:36 +0900

    Update automatic_server_stopper.py

--- automatic_server_stopper.py
+++ automatic_server_stopper.py
@@ -1,12 +1,21 @@
 import subprocess
 import json
+import pprint
+# import time
 import datetime
 import pandas as pd
 import os
+# import utils.myfunc as mf
+
+
+## Threshoulds
+log_min = 30
+mem_used_delta_threshould = 10
+gpu_util_mean_threshould = 50
 
 
 ## Load csv
-csv_path = '/tmp/gpu_utils.csv'
+csv_path = '/tmp/gpu_utils_log.csv'
 exist = os.path.isfile(csv_path)
 if exist:
   df = pd.read_csv(csv_path, parse_dates=['Time'])
@@ -39,7 +48,7 @@ def get_gpu_info(nvidia_smi_path='nvidia-smi', keys=DEFAULT_ATTRIBUTES, no_units
 
     return [ { k: v for k, v in zip(keys, line.split(', ')) } for line in lines ]
 
-# print(get_gpu_info())
+# pprint.pprint(get_gpu_info())
 
 def get_Memory_Usage():
   gpus_info = get_gpu_info()
@@ -64,7 +73,7 @@ def get_GPU_util():
 # print(get_GPU_util())
 
 
-## Get current GPU utility
+## Get current GPU utilities
 mem_used = get_Memory_Usage()
 gpu_util = get_GPU_util()
 now = datetime.datetime.now()
@@ -74,8 +83,7 @@ df_now = pd.Series({"Time":now,
                      })
 
 
-## Update DataFrame
-log_min = 30
+## Update the logging table
 log_start_time = now - datetime.timedelta(minutes=log_min)
 df = df.append(df_now, ignore_index=True)
 
@@ -93,13 +101,11 @@ print(mem_used_delta, gpu_util_mean)
 
 ## Decide whether to shutdown or not
 condition_1 = (log_min < len(df)) # Time
-mem_used_delta_threshould = 10
 condition_2 = (mem_used_delta < mem_used_delta_threshould) # Mem
-gpu_util_mean_threshould = 50
 condition_3 = (gpu_util_mean < gpu_util_mean_threshould) # Util
-
 print(condition_1, condition_2, condition_3)
 # rm /tmp/gpu_utils.csv
 
 if [condition_1, condition_2, condition_3] == [True, True, True]:
+  os.remove(csv_path)
   os.system('shutdown now')
