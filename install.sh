99ca3d5bb0cfc46f3c8d2d0f1ffc1207a815b45e
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-09-01 08:57:25 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+''' This script should be run on your remote machine. '''
+
+# Make your remote machine's shutdown commands available without password.
+sudo chmod u+s /sbin/shutdown
+
+# Put start_instance.sh and automatic_server_stopper.py where your PATH is through. For example,
+sudo cp start_instance.sh automatic_server_stopper.py /usr/local/bin/
+
+# Set cron on you machine.
+sudo cp automatic_server_stopper.cron /etc/cron.d
+sudo chmod 0644 /etc/cron.d/automatic_server_stopper.cron
+
+# rm /tmp/gpu_utils_log.csv during each booting.
+sudo sh -c "echo rm /tmp/gpu_utils_log.csv >> /etc/rc.local"
