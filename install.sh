f1a94c8a5e40f08625cbc0b7c79596d5e1ca7701
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-09-01 09:28:58 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo chmod u+s /sbin/shutdown
 
 # Put start_instance.sh and automatic_server_stopper.py where your PATH is through. For example,
 sudo cp automatic_server_stopper.py /usr/local/bin/ # start_gcp_instance.sh 
+sudo chmod +x /usr/local/bin/automatic_server_stopper.py
 
 # Set cron on you machine.
 sudo cp automatic_server_stopper.cron /etc/cron.d
@@ -13,3 +14,10 @@ sudo chmod 0644 /etc/cron.d/automatic_server_stopper.cron
 
 # rm /tmp/gpu_utils_log.csv during each booting.
 sudo sh -c "echo rm /tmp/gpu_utils_log.csv >> /etc/rc.local"
+sudo chmod +x /etc/rc.local
+
+## Check
+# automatic_server_stopper.py
+# sudo systemctl status crond
+# sudo systemctl restart crond
+# ls /tmp/gpu_utils_log.csv
