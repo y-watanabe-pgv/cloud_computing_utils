c43f96b42c4565f5b75425f8ac00bb8d3f462a39
Yusuke Watanabe <42527473+ywatanabe1989@users.noreply.github.com>
2019-09-01 11:11:04 +0900

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sudo rm /usr/local/bin/automatic_sever_stopper.py
+sudo rm /usr/local/bin/automatic_server_stopper.py
 sudo rm /etc/cron.d/automatic_server_stopper.cron
+export EDITOR
 sudo $EDITOR /etc/rc.local # delete the line "rm /tmp/gpu_utils_log.csv"
