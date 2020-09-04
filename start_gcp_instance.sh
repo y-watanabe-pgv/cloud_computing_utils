ae6baa8f4f433b87d70768f770fe69a958495021
y-watanabe-pgv <68572979+y-watanabe-pgv@users.noreply.github.com>
2020-09-05 03:04:32 +0900

    Update start_gcp_instance.sh

--- start_gcp_instance.sh
+++ start_gcp_instance.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+YOUR_INSTANCE_NAME=?????
+YOUR_INSTANCE_ZONE=????? # us-central1-a
+
+
 started=false
 while ! $started
 do
-    gcloud compute instances start [YOUR_INSTANCE_NAME] --zone=YOUR_INSTANCE_REGION && started=true # us-central1-a
+    gcloud compute instances start $YOUR_INSTANCE_NAME --zone=$YOUR_INSTANCE_ZONE && started=true 
 done
 
 connected=false
 while ! $connected
 do
-    ssh [YOUR_INSTANCE_NAME] && connected=true
+    ssh $YOUR_INSTANCE_NAME && connected=true
 done
